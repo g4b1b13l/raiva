d1130fda28af10920f869a164d54e109c5b19959
g4b1b13l <56329206+g4b1b13l@users.noreply.github.com>
2019-11-01 11:11:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,24 @@ external_stylesheets = ['https://codepen.io/g4b1b13l/pen/VwwrYdL.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-app.layout = html.Div(children=[
-    html.H1(children='AMO TU MO'),
+app.layout =     html.Div(
 
+        [html.H1(children= 'VAI ROLAR HOJE?'),
+       
+        ]
+              , 
 
-
-])
+         style={
+        'font-size': '5pt',
+        #'height': '75px',
+        'margin': '-10px -10px -10px',
+        'background-color': '#ADD8E6',
+        'text-align': 'center',
+        #'border-radius': '2px',
+        #'display': 'flex',
+        #'margin-left': '0',
+        } 
+        ),
 
 
 
