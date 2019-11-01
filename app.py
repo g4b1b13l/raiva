276eab5716dcb9297991b5ab1d50fa7c62e5ba93
g4b1b13l <56329206+g4b1b13l@users.noreply.github.com>
2019-11-01 11:17:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,10 @@ external_stylesheets = ['https://codepen.io/g4b1b13l/pen/VwwrYdL.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-app.layout =     html.Div(
+app.layout = 
 
-        [html.H1(children= 'Geroge eu vi você na cama com Zé'),
+        html.H1(children= 'Geroge eu vi você na cama com Zé')
        
-        ]
-              , 
-
-         style={
-        'font-size': '5pt',
-        #'height': '75px',
-        'margin': '-10px -10px -10px',
-        'background-color': '#ADD8E6',
-        'text-align': 'center',
-        #'border-radius': '2px',
-        #'display': 'flex',
-        #'margin-left': '0',
-        } 
-        ),
 
 
 
