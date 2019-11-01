c4da41d1f19a0d11ce604aa982a80edb5b60b6d5
g4b1b13l <56329206+g4b1b13l@users.noreply.github.com>
2019-11-01 17:39:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import psycopg2 as psy
+
+mydb=psy.connect (
+host='ec2-54-235-100-99.compute-1.amazonaws.com',
+user = 'mhxcrjdnckxtbr',
+password='041d51b54231eb4e36b2a8d58f5ae16bc5cfaab2303d426676580f62e52ebcc1',
+database='d9k1k422mp16r5')
 
 external_stylesheets = ['https://codepen.io/g4b1b13l/pen/VwwrYdL.css']
 
